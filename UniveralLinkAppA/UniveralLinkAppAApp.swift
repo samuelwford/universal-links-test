a65dadc72b8297e537cad7f738fa0f517997ccc2
Samuel Ford <samuel.ford@icloud.com>
2020-11-09 10:33:36 -0500

    Initial Commit

---
+++ UniveralLinkAppA/UniveralLinkAppAApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniveralLinkAppAApp.swift
+//  UniveralLinkAppA
+//
+//  Created by Samuel Ford on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct UniveralLinkAppAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
